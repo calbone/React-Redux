0069bfde0380d502459f57f97c937b9d400ea7a1
yu-ku-air <y.kuma1229@gmail.com>
2018-05-09 01:22:37 +0900

    modify containers, flowconfig

--- src/contaniers/SupportTop.js
+++ src/contaniers/SupportTop.js
@@ -3,22 +3,9 @@ import { connect } from 'react-redux';
 import SupportTop from '@/components/pages/SupportTop';
 import { fetchService } from '@/actions/tasksActionCreator';
 
-function mapStateToProps({ serviceList }) {
-  return {
-    serviceList
-  };
-}
-
-function mapDispatchToProps(dispatch) {
-  return {
-    fetchService() {
-      dispatch(fetchService());
-    }
-  };
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(
-  class SupportTopContainer extends Component {
+export default connect (
+  state => ({ serviceList: state.serviceList }), { fetchService })
+  (class SupportTopContainer extends Component {
     componentDidMount() {
       this.props.fetchService();
     }
