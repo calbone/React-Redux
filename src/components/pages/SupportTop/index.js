2ffe775e87bdeacfc8a16b5b7b6e0951f8340bce
yu-ku-air <y.kuma1229@gmail.com>
2018-06-05 21:43:30 +0900

    modify component

--- src/components/pages/SupportTop/index.js
+++ src/components/pages/SupportTop/index.js
@@ -23,7 +23,7 @@ export default props => {
               <li key={i}>
                 <div className={Styles.accordionWarp}>
                   <div
-                    id={item.serviceId}
+                    id={ite.serviceId}
                     className={Styles.accordionTitleWrap}
                   >
                     <ServiceIcon serviceName={item.data.serviceIcon} />
