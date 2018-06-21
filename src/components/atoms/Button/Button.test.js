7668e32d1f93caee0b90fb8d9a4af832c57ae4c4
yu-ku-air <y.kuma1229@gmail.com>
2018-06-21 22:55:45 +0900

    modify Button.test.js

--- src/components/atoms/Button/Button.test.js
+++ src/components/atoms/Button/Button.test.js
@@ -3,7 +3,7 @@ import { shallow } from 'enzyme';
 import PrimaryButton from './index';
 
 test('Buttonコンポーネント', () => {
-  const text = 'ほげほげ';
+  const text = 'ふがふが';
   const wrapper = shallow(<PrimaryButton>{text}</PrimaryButton>);
 
   expect(wrapper.contains(text)).toEqual(true);
